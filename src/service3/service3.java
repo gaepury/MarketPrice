66ce115a9b1fb0a00198b2bab60735d1d74e8798
njh190505 <gmail.com>
2016-12-06 21:58:58 +0900

    길찾기 완료. service 3 코드 복구.

--- src/service3/service3.java
+++ src/service3/service3.java
@@ -10,32 +10,31 @@ import javax.ws.rs.Produces;
 
 @Path("/dir")
 public class service3 {
-	private static final String DEFAULT_URL =" https://www.google.co.kr/maps/dir/";
-	
-	
+	private static final String DEFAULT_URL = "https://www.google.co.kr/maps/dir/";
+
 	@GET
 	@Produces("application/json; charset=UTF-8")
 	@Path("/{lon}-{lat}/{name}")
-	public String loadSearch(@PathParam("lon") double lon, @PathParam("lat") double lat, @PathParam("name") String name){
-
+	public String loadSearch(@PathParam("lon") double lon, @PathParam("lat") double lat,
+			@PathParam("name") String name) {
 
 		String dirurl;
-		String jsonResult="{ \"result\" : ";
+		String jsonResult = "{ \"result\" : ";
 		URL url;
 		try {
-			url = new URL(DEFAULT_URL+lat+","+lon+"/"+name);
-			
-			dirurl=url.toString();
-			
-			jsonResult+= "\""+dirurl+"\"}";
-			
+			url = new URL(DEFAULT_URL + lat + "," + lon + "/" + name);
+
+			dirurl = url.toString();
+
+			jsonResult += "\"" + dirurl + "\"}";
+
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
-			jsonResult+= "\"fail\"}";
+			jsonResult += "\"fail\"}";
 		}
-		
+
 		return jsonResult;
-		
+
 	}
 
 }
